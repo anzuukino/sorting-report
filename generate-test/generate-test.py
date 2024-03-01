e12e7fda0d9167687909b74dae0aee1693349456
anzuukino <tathacklt1@gmail.com>
2024-03-01 15:20:22 +0700

    test-generating

--- generate-test/generate-test.py
+++ generate-test/generate-test.py
@@ -4,8 +4,13 @@ np.random.seed(1337)
 
 
 for i in range(1,11):
-    file = f"input{i}.inp"
-    random_number_array = np.random.rand(1000000)
+    file = f"generate-test/input{i}.inp"
+    random_number_array = np.random.uniform(0,1337,1000000)
+    if i == 1:
+        random_number_array.sort()
+    if i == 2:
+        random_number_array.sort()
+        random_number_array = random_number_array[::-1]
     with open(file,'w') as filex:
         for j in random_number_array:
             filex.write(f"{j}\n")
